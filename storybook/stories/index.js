8900c8ad0af637e696d63184598f8b4500d98af3
Mohamed Hamed <Mohamed.Hamed1@vodafone.com>
2017-10-26 09:37:53 +1300

    Adding Carousel stories

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -1,36 +1,167 @@
 import React from "react";
-import { View, Text } from "react-native";
-
+import { Text, View, Dimensions, ScrollView } from "react-native";
 import { storiesOf } from "@storybook/react-native";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
-
 import Button from "./Button";
 import CenterView from "./CenterView";
 import Welcome from "./Welcome";
+import Carousel from "./Carousel/Carousel";
+import styles from "./stories/storiesStyles";
+const {
+  selectedDataStyle,
+  subTextStyle,
+  selectedMinStyle,
+  selectedTxtStyle
+} = styles;
 
+const renderControl = () => {
+  return (
+    <Carousel.Control
+      style={{ alignItems: "flex-end" }}
+      dot={
+        <Text
+          style={{
+            backgroundColor: "rgba(0, 0, 0, 0)",
+            color: "#5bc0de",
+            padding: 4
+          }}
+        >
+          □
+        </Text>
+      }
+      activeDot={
+        <Text
+          style={{
+            backgroundColor: "rgba(0, 0, 0, 0)",
+            color: "#5bc0de",
+            padding: 4
+          }}
+        >
+          ■
+        </Text>
+      }
+    />
+  );
+};
 storiesOf("Welcome", module).add("Home", () => (
   <Welcome showApp={linkTo("Button")} />
 ));
-
 storiesOf("Button", module)
   .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
-  .add(" Button with text", () => (
-    <View >
-      <Button style={styles.containerStyle} text="Disabled Hero" type="Disabled" on="Hero" />
+  .add("Button with text", () => (
+    <View>
+      <Button
+        style={styles.containerStyle}
+        text="Disabled Hero"
+        type="Disabled"
+        on="Hero"
+      />
 
-      <Button style={styles.containerStyle} text="Primary Overlay" type="Primary" on="Overlay" />
+      <Button
+        style={styles.containerStyle}
+        text="Primary Overlay"
+        type="Primary"
+        on="Overlay"
+      />
 
-      <Button style={styles.containerStyle} text="Loading Overlay" type="Loading" on="Overlay" />
+      <Button
+        style={styles.containerStyle}
+        text="Loading Overlay"
+        type="Loading"
+        on="Overlay"
+      />
 
-      <Button style={styles.containerStyle} text="Tertiary Overlay" type="Tertiary" on="PageNudge" />
+      <Button
+        style={styles.containerStyle}
+        text="Tertiary Overlay"
+        type="Tertiary"
+        on="PageNudge"
+      />
 
-      <Button style={styles.containerStyle}text="Secondary Card" type="Secondary" on="Card" />
+      <Button
+        style={styles.containerStyle}
+        text="Secondary Card"
+        type="Secondary"
+        on="Card"
+      />
     </View>
   ));
-
-  const styles={
-    containerStyle:{
-      marginBottom: 20
-    }
-  }
+storiesOf("carousel", module)
+  .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
+  .add("default Carousel", () => (
+    <View style={{ flex: 1 }}>
+      <Carousel style={{ height: 238, width: 238 }}>
+        <View style={{ flexDirection: "row" }}>
+          <Text style={selectedDataStyle}>100MB</Text>
+          <Text style={subTextStyle}>DATA</Text>
+        </View>
+        <View style={{ flexDirection: "row" }}>
+          <Text style={selectedMinStyle}>300 </Text>
+          <Text style={subTextStyle}>MIN</Text>
+        </View>
+        <View style={{ flexDirection: "row" }}>
+          <Text style={selectedTxtStyle}>250 </Text>
+          <Text style={subTextStyle}>TXT</Text>
+        </View>
+      </Carousel>
+    </View>
+  ))
+  .add("carousel  without timeout", () => (
+    <View>
+      <View style={{ flex: 1 }}>
+        <Carousel style={{ height: 238, width: 238 }} carousel={false}>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={selectedDataStyle}>100MB</Text>
+            <Text style={subTextStyle}>DATA</Text>
+          </View>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={selectedMinStyle}>300 </Text>
+            <Text style={subTextStyle}>MIN</Text>
+          </View>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={selectedTxtStyle}>250 </Text>
+            <Text style={subTextStyle}>TXT</Text>
+          </View>
+        </Carousel>
+      </View>
+    </View>
+  ))
+  .add("carousel  With timeout loop 1 time", () => (
+    <View>
+      <View style={{ flex: 1 }}>
+        <Carousel style={{ height: 238, width: 238 }} cycle={false}>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={selectedDataStyle}>100MB</Text>
+            <Text style={subTextStyle}>DATA</Text>
+          </View>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={selectedMinStyle}>300 </Text>
+            <Text style={subTextStyle}>MIN</Text>
+          </View>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={selectedTxtStyle}>250 </Text>
+            <Text style={subTextStyle}>TXT</Text>
+          </View>
+        </Carousel>
+      </View>
+    </View>
+  ))
+  .add("carousel  With controls", () => (
+    <View style={{ flex: 1 }}>
+      <Carousel style={{ height: 100, width: 200 }} control={renderControl()}>
+        <View style={{ flexDirection: "row" }}>
+          <Text style={selectedDataStyle}>100MB</Text>
+          <Text style={subTextStyle}>DATA</Text>
+        </View>
+        <View style={{ flexDirection: "row" }}>
+          <Text style={selectedMinStyle}>300 </Text>
+          <Text style={subTextStyle}>MIN</Text>
+        </View>
+        <View style={{ flexDirection: "row" }}>
+          <Text style={selectedTxtStyle}>250 </Text>
+          <Text style={subTextStyle}>TXT</Text>
+        </View>
+      </Carousel>
+    </View>
+  ));
---
+++ storybook/stories/stories/storiesStyles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native'
+
+
+export default StyleSheet.create({
+    selectedDataStyle: {
+        fontSize: 34,
+        color: '#e60000'
+      },
+      subTextStyle: {
+        fontSize: 14
+      },
+      selectedMinStyle: {
+        fontSize: 34,
+        color: '#fecb00'
+      },
+      selectedTxtStyle: {
+        fontSize: 34,
+        color: '#00b0ca'
+      }
+})
